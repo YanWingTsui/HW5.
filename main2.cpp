540aa7637807047425fe4bd186115aef9f042d2b
YanWingTsui <47301570+YanWingTsui@users.noreply.github.com>
2019-05-26 11:32:50 -0400

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -1,16 +1,20 @@
 #include "queue.h"
 #include <vector>
-#include <cstdlib> // for random so that i can create random times
-#include <time.h> //for random so that i can create random times
+#include <cstdlib> 
+#include <time.h> 
 
-void shiftForward(std::vector<nodespace::QList> &l){
-    for(int i = 0; i < 5; i++){
+void shiftForward(std::vector<nodespace::QList> &l)
+{
+    for(int i = 0; i < 5; i++)
+    {
         int holder = l[i].front() - 1;
         l[i].pop();
         if(holder > 0)
             l[i].push(holder);
-        if(!l[i].empty()){
-            for(int j = 0; j < l[i].size() - 1; j++){
+        if(!l[i].empty())
+        {
+            for(int j = 0; j < l[i].size() - 1; j++)
+            {
                 l[i].push(l[i].front());
                 l[i].pop();
             }
@@ -19,29 +23,36 @@ void shiftForward(std::vector<nodespace::QList> &l){
 }
 
 
-void swapOver(std::vector<nodespace::QList> &l){
+void swapOver(std::vector<nodespace::QList> &l)
+{
     bool isDone = false;
-    while(!isDone){
+    while(!isDone)
+    {
         int posSmall = 99;
         int small = 99;
-        for(int i = 0; i < 5; i++){
-            if(l[i].size() < small){
+        for(int i = 0; i < 5; i++)
+        {
+            if(l[i].size() < small)
+            {
                 small = l[i].size();
                 posSmall = i;
             }
         }
         int posLarge = 0;
         int large = 0;
-        for(int i = 0; i < 5; i++){
-            if(l[i].size() > large){
+        for(int i = 0; i < 5; i++)
+        {
+            if(l[i].size() > large)
+            {
                 large = l[i].size();
                 posLarge = i;
             }
         }
         
-        if(large - small > 1){
-            //first i get the last customer to the front of the queue
-            for(int i = 0; i < l[posLarge].size() - 1; i++){
+        if(large - small > 1)
+        {
+            for(int i = 0; i < l[posLarge].size() - 1; i++)
+            {
                 int temp = l[posLarge].front();
                 l[posLarge].push(temp);
                 l[posLarge].pop();
@@ -50,7 +61,8 @@ void swapOver(std::vector<nodespace::QList> &l){
             std::cout << "A customer of check-out time " << l[posLarge].front() << "-minutes has swapped over to line " << posSmall + 1 << " from line " << posLarge + 1 << "\n";
             l[posSmall].push(l[posLarge].front());
             l[posLarge].pop();
-            for(int i = 0; i < 5; i++){
+            for(int i = 0; i < 5; i++)
+            {
                 l[i].functionIMadeForPrinting();
             }
             std::cout << "\n";
@@ -60,7 +72,8 @@ void swapOver(std::vector<nodespace::QList> &l){
     }
 }
 
-void groceryStore(){
+void groceryStore()
+{
  
     srand(time(NULL));
     std::vector<nodespace::QList> lines;
@@ -68,8 +81,7 @@ void groceryStore(){
         nodespace::QList temp;
         lines.push_back(temp);
     }
-    //was not sure how long this program should run for so i
-    //let the user enter decide
+   
     char cont = 'y';
     while(cont == 'y'){
        
@@ -78,45 +90,42 @@ void groceryStore(){
         std::cout << "-----------------------------------------------------------------------------------------------------------------------";
         std::cout << "\n\n\n";
         std::cout << numberOfCustomers << " new customers are ready to check-out:\n\n";
-        for(int i = 0; i < numberOfCustomers; i++){
-            //creating a customer of a wait time between 1 and 3
+        for(int i = 0; i < numberOfCustomers; i++)
+        {
             int customer = rand() % 10 + 1;
-            //this gets the position of the shortest line in the queue
-            //if they are all the same then the first line is chosen
             int pos = 99;
             int shortest = 99;
             for(int i = 0; i < 5; i++){
-                if(lines[i].size() < shortest){
+                if(lines[i].size() < shortest)
+                {
                     shortest = lines[i].size();
                     pos = i;
                 }
             }
-            //shortest line is at lines[pos]
+         
             std::cout << "Customer with a check-out time needed of " << customer << "-minutes joined line " << pos+1 << "\n";
             lines[pos].push(customer);
-            for(int i = 0; i < 5; i++){
+            for(int i = 0; i < 5; i++)
+            {
                 lines[i].functionIMadeForPrinting();
             }
             std::cout << "\n";
         }
-        //calls the function to simulate a minute passing
+    
         std::cout << "One minute has passed\n";
         shiftForward(lines);
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 5; i++)
+        {
             lines[i].functionIMadeForPrinting();
         }
         std::cout << "\n";    
-        //calling the function that lets customers swap over to shorter lines
         swapOver(lines);
-        //asking the user if they want to continue
         std::cout << "Continue simulation? (y/n) ";
         std::cin >> cont;
     }
 }
 
 int main(){
-    //note that the printed numbers are lines from left to right with the 
-    //left-most number being the start of that line
     std::cout << "The program currently has the following properties:\nA random number of customers will join the lines every \"minute\"\n";
     std::cout << "The customers will have random values which represents the minutes they will take once they get to the register\n";
     std::cout << "The new customers will join the shortest line, if all are of equal length they will join the first one\n";
